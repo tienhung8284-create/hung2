029dc515e68c5423d3c4ea853579d601e80d062b
tienhung8284-create <tienhung8284@gmail.com>
2026-01-21 20:25:25 +0700

    Delete pkg directory

--- pkg/__init__.py
+++
@@ -1,4 +0,0 @@
-print('Chương trình game ABC')
-
-list_gun = ['Loai 1','Loai 2','Loai 3']
-list_bullet = ['Dan loai 1','Dan loai 2','Dan loai 3']
--- pkg/mod1.py
+++
@@ -1,9 +0,0 @@
-# Nhiệm vụ Module 1: Load dữ liệu + Tạo khách hàng
-import pkg
-def load_data():
-    print('Đã tải xong súng')
-def load_data100():
-    print('Đã tải xong các loại đạn')
-
-class Customer:
-    pass
